9dd4786d12562ed06aa7337ef9586703c8dac2cf
Timchen7 <timchen7@yahoo.com>
2017-03-08 16:36:31 +0000

    Fix user.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,8 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+   # ROR1 for Heroku
    provider :facebook, '605593792965258', 'af1d23b14a7e59751716913dbca3aa80', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+   # ROR2 for Local
+   #provider :facebook, '1908361179399739', 'c17b3208911259bd04e91458dee67df7', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
   end
